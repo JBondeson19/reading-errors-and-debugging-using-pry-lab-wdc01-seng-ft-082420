55af7c331c694ffdfd8fcf2ce19e5fa3e7bdd1de
James P Bondeson <jpbond58@gmail.com>
2020-08-06 03:28:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,13 +4,15 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-
+binding.pry
 def state_log(star_date)
  "Captain's Log, star date #{star_date}."
 end
 
 
 def engage
+  binding.pry
   date = generate_star_date
   puts state_log(date)
 end
+binding.pry
